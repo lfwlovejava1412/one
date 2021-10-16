6ad030c0ac18732066a2c99e56ab18be0659c843
lfwlovejava1412 <1498292726@qq.com>
2021-10-16 11:20:56 +0800

    提交的第一个文件

--- client/src/main/java/com/bwie/ClientApplication.java
+++ client/src/main/java/com/bwie/ClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
 public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class,args);
